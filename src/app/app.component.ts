1f46a06b507027e3dd32c28538061f4956b0c83c
vlexx91 <alemartinenghi@gmail.com>
2025-01-16 14:26:24 +0100

    ejercicio3 completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Ejercicio1Component} from './ejercicio1/ejercicio1.component';
 import {Ejercicio2Component} from './ejercicio2/ejercicio2.component';
+import {Ejercicio3Component} from './ejercicio3/ejercicio3.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Ejercicio1Component, Ejercicio2Component],
+  imports: [RouterOutlet, Ejercicio1Component, Ejercicio2Component, Ejercicio3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
