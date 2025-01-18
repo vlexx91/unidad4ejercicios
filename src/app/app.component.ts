08642f9452dc74f8daca026bc1fb37dc9d92f01c
vlexx91 <alemartinenghi@gmail.com>
2025-01-18 18:29:25 +0100

    ejercicios 5, 6 y 7 realizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import {Ejercicio2Component} from './ejercicio2/ejercicio2.component';
 import {Ejercicio3Component} from './ejercicio3/ejercicio3.component';
 import {Ejercicio4Component} from './ejercicio4/ejercicio4.component';
 import {Ejercicio5Component} from './ejercicio5/ejercicio5.component';
+import {Ejercicio6Component} from './ejercicio6/ejercicio6.component';
+import {Ejercicio7Component} from './ejercicio7/ejercicio7.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Ejercicio1Component, Ejercicio2Component, Ejercicio3Component, Ejercicio4Component,
-  Ejercicio5Component],
+  Ejercicio5Component, Ejercicio6Component, Ejercicio7Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
